bf07e0fcac13b4676c8c2e30b6746bc9c3830bc2
Faris Ahmad Zyoud <farisahmadzyoud@gmail.com>
2021-06-20 12:33:55 +0300

    My Second Task

---
+++ MyStack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class MyStack
+	{
+		
+
+		public void PushTostack(Stack<string> myStack)
+        {
+
+            myStack.Push("one");
+			myStack.Push("two");
+			myStack.Push("three");
+			myStack.Push("four");
+			myStack.Push("five");
+			myStack.Push("six");
+			Console.Write("Total number of elements in the Stack are : ");
+			Console.WriteLine(myStack.Count);
+
+			Console.Write("This is Push operation ");
+
+
+
+		}
+
+		internal void pushtostack(MyStack stack1)
+        {
+            throw new NotImplementedException();
+        }
+        public void PeekFromStack(Stack<string> myStack)
+       {
+
+
+		Console.WriteLine("Element at the top is : " + myStack.Peek());
+		Console.WriteLine("The Count After Peek is : "+ myStack.Count);
+
+
+
+		}
+		public void PopFromStack(Stack<string> myStack)
+		{
+
+			Console.Write(myStack.Pop());
+			Console.WriteLine( "The Count After Pop  Element is "+ myStack.Count);
+			
+
+
+
+		}
+		public void ClearStack(Stack<string> myStack)
+		{
+
+			myStack.Clear();
+			Console.WriteLine("The Count After claering Element is  " + myStack.Count);
+
+
+
+
+		}
+
+
+
+
+
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// C# code to insert an object
+// at the top of the Stack
+using System;
+using System.Collections.Generic;
+
+
+namespace Stack { 
+public class Tak2 {
+
+	// Driver code
+	public static void Main()
+	{
+			Stack<string> myStack = new Stack<string>();
+
+			// Creating a Stack of strings\
+			var stack1 = new MyStack();
+			stack1.PushTostack(myStack);
+			stack1.PeekFromStack(myStack);
+			stack1.PopFromStack(myStack);
+			stack1.ClearStack(myStack);
+		      
+
+		
+
+
+
+		}
+	}
+}
